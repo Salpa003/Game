edfd1e19ecfbf34ec0e4fe2ac06b7095f60afe89
Salpagarov Ruslan <salpagarovruslan11@gmail.com>
2024-07-27 16:08:49 +0300

    Robot hahahah in main

--- Main.java
+++ Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
 Robot robot=new Robot("R34");
         System.out.println(robot.getName());
+
+
+        System.out.println("eeee hahaahahahahha");
     }
 }
