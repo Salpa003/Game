e8b5ef1b325788900ebcb32872d7491922d5f035
Salpagarov Ruslan <salpagarovruslan11@gmail.com>
2024-07-27 16:02:20 +0300

    rename "R34" to "Tiger 228"

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
 Robot robot=new Robot("R34");
         System.out.println(robot.getName());
+        robot.setName("Tiger 228");
+        System.out.println(robot.getName());
     }
 }
